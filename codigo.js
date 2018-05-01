5823fe9f33185149444f6f4ddcfcc69f651f2316
rfpg11 <36582690+rfpg11@users.noreply.github.com>
2018-05-01 21:51:24 +0100

    1.0

---
+++ codigo.js
@@ -0,0 +1,28 @@
+function Init() { //Função inicial
+
+    function youtube(youtuber, conteudo, subscritores, videosSemanais){
+		this.youtuber = youtuber;
+		this.conteudo  = conteudo;
+		this.subscritores = subscritores;
+		this.videosSemanais = videosSemanais;
+		this.informacoes = function() {
+			document.body.innerHTML += "Youtuber: ".bold() + this.youtuber + "<br>";
+			document.body.innerHTML += "Conteúdo: ".bold() + this.conteudo + "<br>";
+			document.body.innerHTML += "Ele tem " + this.subscritores.bold() + " subscritores" + "<br>";
+			document.body.innerHTML += "Ele faz " + this.videosSemanais.bold() + " vídeos por dia" + "<br><br>";
+		}
+	}
+      
+   
+    var Wuant = new youtube("Wuant", "Vlog", "2.861.560", "4");
+	Wuant.informacoes();
+	
+	
+	var RicFazeres = new youtube("RicFazeres", "Gaming", "573.592" , "6");
+	RicFazeres.informacoes();
+	
+	
+	var MoceDumCabreste = new youtube("Môce Dum Cabreste", "Comédia", "185.057", "2");
+	MoceDumCabreste.informacoes();
+
+}
